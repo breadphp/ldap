b97c852257aaf38e3b5ecf263c9e7f07f51ed914
Giovanni Lovato <heruan@aldu.net>
2013-10-08 17:24:09 +0200

    Removed objectClass property from Top trait

--- src/Bread/LDAP/Traits/Top.php
+++ src/Bread/LDAP/Traits/Top.php
@@ -7,15 +7,5 @@ use Bread\Configuration;
 trait Top
 {
     use Internal;
-    
-    protected $objectClass;
 }
 
-Configuration\Manager::defaults('Bread\LDAP\Traits\Top', array(
-    'properties' => array(
-        'objectClass' => array(
-            'required' => true,
-            'multiple' => true
-        )
-    )
-));
